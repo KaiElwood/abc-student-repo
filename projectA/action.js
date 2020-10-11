9dd3e36137113d936b0f6e310ba1f8ef5fd7a1e5
Kai Elwood-Dieu <ked407@nyu.edu>
2020-10-11 17:23:05 -0600

    updated js

--- projectA/action.js
+++ projectA/action.js
@@ -8,3 +8,18 @@ function deferVideo() {
 }
 
 window.onLoad = deferVideo();
+
+
+// first, setinterval and add px to video at interval. then, after ellapsed time set visibility (or display) to none, start next cat video at previous position (plus or minus offset)
+
+function catsWalk(target, speed, direction){
+    setInterval(speed, function(direction){
+        $(target).css("left", 
+    })
+
+    $("#cat-deck").css("left", 
+    
+    function(){
+        return ($(this).css("left") + 5);
+    })
+}
