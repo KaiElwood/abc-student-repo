d71f38634bf98236a820de451b243e86cc69fcc4
Kai Elwood-Dieu <ked407@nyu.edu>
2020-10-12 18:12:51 -0600

    added functioning cat movement

--- projectA/action.js
+++ projectA/action.js
@@ -4,42 +4,119 @@ function deferVideo() {
         $(this).attr("src", sourcefile);
         var video = this.parentElement;
         video.load();
+        // video.play();
     })
 }
 
 window.onLoad = deferVideo();
 
+const foodBowl = $("#foodBowl")
+
+
+foodBowl.on("click", function(){
+    $(foodBowl).addClass("shake");
+    var audio = $("#mysoundclip")[0];
+    audio.currentTime = 0;
+    audio.play();
+    var num = Math.random();
+    if ( num < .4){
+        catFromLeft();
+    } else if (num <.7){
+        catFromRight();
+    } else {
+        catFromBottom();
+    }
+    setTimeout(function(){$(foodBowl).removeClass("shake");}, 600);
+})
+
 // first, setinterval and add px to video at interval. then, after ellapsed time set visibility (or display) to none, start next cat video at previous position (plus or minus offset)
 
 // time in # of repeats, speed in millis
 
+// i need time because I need to know when to start and stop videos
+// I need speed to be a function of the width of the screen
+
 function catsWalk(target, speed, direction, time){
+    $(`#${target}`).get(0).currentTime = 0;
     $(`#${target}`).trigger("play");
+    $(`#${target}`).css("visibility", "visible");
     var count = 0;
     var endPos;
     var interval = setInterval(function(){
         var currentPosition = $(`#${target}`).position();
-        $(`#${target}`).css(`${direction}`, currentPosition.left + .5);
+
+        $(`#${target}`).css(`${direction}`, currentPosition.left + ((window.innerWidth/1000) * speed));
         if (count > time){
             clearInterval(interval);
             $(`#${target}`).trigger("pause");
+            $(`#${target}`).css("visibility", "hidden");
             endPos = currentPosition;
             console.log(endPos);
             // return endPos;
         }
         count ++;
-    }, speed)
-    return interval;
+    }, 10)
+    // await endPos;
 };
 
-var cat1 = catsWalk("catDeck", 10, "left", 400);
-// var target = "catDeck"
-// var currentPosition = $(`#${target}`).position();
-// console.log("moved" + "    " + $("#catDeck").css(`left`) + "       " + currentPosition.left)
+function catFromLeft(){
+    $("#catDeck").css("left", "-500px");
+    var cat1 = $("#catDeck");
+    var cat2 = $("#catBlack");
+    cat1.get(0).currentTime = 0;
+    cat2.get(0).currentTime = 0;
+    var cat1T = 400;
+    catsWalk("catDeck", 2, "left", cat1T);
 
-// setInterval(function () {
-//     var currentPosition = $("#catDeck").position();
-    
-//     console.log("moved" + "    " + $("#catDeck").css(`left`) + "       " + currentPosition.left );
-//         $("#catDeck").css(`left`, currentPosition.left + .5)
-// }, 10);
+    // cat2.css("left", cat1Pos.left);
+
+    setTimeout(function(){
+        // debugger;
+        // console.log($("#catDeck").position());
+        var cat1Pos = cat1.position();
+        cat2.css("left", cat1Pos.left - 100);
+        catsWalk("catBlack", .5, "left", cat1T);
+    }, cat1T * 10);
+}
+
+function catFromRight(){
+    var cat1 = $("#sandCat");
+    var cat2 = $("#highCat");
+    // cat1.get(0).currentTime = 0;
+    // cat2.get(0).currentTime = 0;
+    var cat1T = 180;
+    cat1.css("left", "80vw");
+    catsWalk("sandCat", -2, "left", cat1T);
+
+    // cat2.css("left", cat1Pos.left);
+
+    setTimeout(function(){
+        // debugger;
+        var cat1Pos = cat1.position();
+        cat2.css("left", cat1Pos.left - 350);
+        console.log($("#sandCat").position());
+        console.log($("#highCat").position());
+        catsWalk("highCat", -.5, "left", 300);
+    }, cat1T * 10);
+}
+
+function catFromBottom(){
+    var cat1 = $("#leashCat");
+    var cat2 = $("#treadmillCat");
+    // cat1.get(0).currentTime = 0;
+    // cat2.get(0).currentTime = 0;
+    var cat1T = 340;
+    cat1.css("left", "-200px");
+    catsWalk("leashCat", .2, "left", cat1T);
+
+    // cat2.css("left", cat1Pos.left);
+
+    setTimeout(function(){
+        // debugger;
+        var cat1Pos = cat1.position();
+        cat2.css("left", cat1Pos.left + 785);
+        // console.log($("#sandCat").position());
+        // console.log($("#highCat").position());
+        catsWalk("treadmillCat", .5, "left", 400);
+    }, cat1T * 10);
+}
