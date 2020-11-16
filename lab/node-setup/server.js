aa5bc61a620064ba225de6ffc7bac3a194340d3e
KED <kaipie3@gmail.com>
2020-11-16 15:03:58 -0700

    added ejs and basic structure

--- lab/node-setup/server.js
+++ lab/node-setup/server.js
@@ -2,14 +2,20 @@
 
 // requires express
 const express = require('express');
+var path = require("path");
 const app = express();
 
 // opens local port on computer
-const port = 3000;
+var port = process.env.PORT || 3000;
 
-// for every request to the url without any addition specifications
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// set view engine as EJS for easy page building
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
+// set public folder
+app.use(express.static('./public'));
+app.get('/', (req, res) => {
+    res.render('pages/home')
 });
 
 // if there is a request to the port, console that the server is active
